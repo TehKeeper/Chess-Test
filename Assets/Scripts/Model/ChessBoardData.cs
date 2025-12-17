d2981a9dad85ddd741aa78e49af0cd95564c9a03
TehKeeper <lexmechanicum@gmail.com>
2025-12-17 19:22:17 +0300

    Meta files
    Chess board Data

---
+++ Assets/Scripts/Model/ChessBoardData.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections.Generic;
+using Model.Data;
+using UnityEngine;
+
+namespace Model {
+    [System.Serializable]
+    public class ChessBoardData {
+        public Dictionary<Vector2Int, FigData> BoardState = new ();
+
+        public bool _currentTurnBlack;
+
+        private static readonly FigType[] MainPieces = new[] {
+            FigType.Rook, FigType.Knight, FigType.Bishop, FigType.Queen, FigType.King, FigType.Bishop,
+            FigType.Knight, FigType.Rook,
+        };
+
+        public void ResetBoard() {
+            _currentTurnBlack = false;
+
+            if (BoardState.Count == 0) {
+                for (int len = 0; len < 64; len++) {
+                    BoardState[new Vector2Int(len % 8, len / 8)] = default;
+                }
+            }
+            else {
+                foreach (Vector2Int key in BoardState.Keys) {
+                    BoardState[key] = default;
+                }
+            }
+        }
+
+        public void SetupStandardGame() {
+            ResetBoard();
+
+            for (int i = 0; i < 8; i++) {
+                PutToBoard(MainPieces[i], false, new Vector2Int(i, 0));
+                PutToBoard(FigType.Pawn, false, new Vector2Int(i, 1));
+                
+                PutToBoard(MainPieces[i], true, new Vector2Int(i, 7));
+                PutToBoard(FigType.Pawn, true, new Vector2Int(i, 6));
+            }
+        }
+
+        public void PutToBoard(FigType type, bool isBlack, Vector2Int position) {
+            FigData piece = new FigData(type, isBlack, position);
+            BoardState[position] = piece;
+        }
+
+        public bool IsValidMove(Vector2Int startPoint, Vector2Int endPoint) {
+            if (!IsInBounds(startPoint) || !IsInBounds(endPoint)) return false;
+            if (startPoint == endPoint) return false;
+
+            FigData piece = GetFigAt(startPoint);
+            if (piece.Type == FigType.None) return false;
+
+            FigData target = GetFigAt(endPoint);
+            if (target.Type != FigType.None && target.IsBlack == piece.IsBlack) return false;
+
+            return true;
+        }
+
+        public void MovePiece(Vector2Int startPoint, Vector2Int endPoint) {
+            FigData piece = GetFigAt(startPoint);
+            if (piece.Type == FigType.None) return;
+            
+            if(!IsValidMove(startPoint, endPoint))
+                return;
+            
+            BoardState[startPoint] = new FigData(FigType.None, false, startPoint);
+            piece.Coordinates = endPoint;
+            BoardState[endPoint] = piece;
+            
+            //invoke here putting figure to graveyard later
+
+            _currentTurnBlack = !_currentTurnBlack;
+        }
+
+        public FigData GetFigAt(Vector2Int position) {
+            return BoardState.TryGetValue(position, out var piece) ? piece : default;
+        }
+
+        private bool IsInBounds(Vector2Int position) {
+            return position.x is >= 0 and < 8 && position.y is >= 0 and < 8;
+        }
+    }
+}
