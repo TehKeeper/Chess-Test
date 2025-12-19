1c7f06806a5104855420417fe7ee2af7d4e75fdc
TehKeeper <lexmechanicum@gmail.com>
2025-12-19 12:47:16 +0300

    Chess board save wrapper

---
+++ Assets/Scripts/Model/Data/ChessBoardDataWrapper.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Model.Data {
+    [System.Serializable]
+    internal struct ChessBoardDataWrapper {
+        public bool CurrentTurn;
+        public FigData[] ChessPieces;
+
+        public ChessBoardDataWrapper(bool currentTurn, Dictionary<Vector2Int, FigData> boardState) {
+            List<FigData> tmpFigData = new();
+            foreach (FigData figure in boardState.Values) {
+                tmpFigData.Add(figure);
+            }
+
+            ChessPieces = tmpFigData.ToArray();
+            CurrentTurn = currentTurn;
+        }
+    }
+}
